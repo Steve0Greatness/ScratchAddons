30ebc425b45212e80502e1da3ea756fdc68a5604
Steve0Greatness <steve0greatnessiscool@gmail.com>
2024-02-23 02:11:07 -0800

    Basic functionallity: show title, url, thumbnail, modified

--- addons/recently-viewed/userscript.js
+++ addons/recently-viewed/userscript.js
@@ -6,7 +6,7 @@ export default async function ({ addon, console }) {
 
 	const Username = await addon.auth.fetchUsername();
 	const RecentlyViewedProjects = await (await fetch(
-		`https://api.scratch.mit.edu/users/${Username}/recentlyviewed`,
+		`https://api.scratch.mit.edu/users/${Username}/projects/recentlyviewed?limit=20&offset=0`,
 		{
 			headers: {
 				"X-Token": XToken
@@ -35,12 +35,31 @@ export default async function ({ addon, console }) {
 	RecentlyViewedCountHolder.innerText = RecentlyViewedProjects.length;
 	console.log(RecentlyViewedProjects)
 
-	const MediaList = document.querySelector("ul.media-list");
+	const MediaListHolder = await addon.tab.waitForElement(".media-list[data-content]");
+
+	const MediaList = document.createElement("ul");
+	MediaList.classList.add("media-list");
 
 	RecentlyViewedAnchor.addEventListener("click", OpenRecentlyViewedTab);
 
-	function OpenRecentlyViewedTab(ProjectObject) {
+	function OpenRecentlyViewedTab() {
+		console.log("Wipping previous medialist");
+		MediaListHolder.innerHTML = "";
+		MediaListHolder.appendChild(MediaList);
+		for (let index = 0; index < RecentlyViewedProjects.length; index++) {
+			const ProjectObject = RecentlyViewedProjects[index];
+			const MediaItem = CreateMediaItem(ProjectObject);
+			MediaList.append(MediaItem);
+		}
+	}
+
+	/**
+	 * Creates a li element for ul.media-list in mystuff
+	 * @param {Object} ProjectObject
+	 */
+	function CreateMediaItem(ProjectObject) {
 		const href = `/projects/${ProjectObject.id}/`;
+		const modified = new Date(ProjectObject.history.modified);
 
 
 		const Holder = document.createElement("li");
@@ -68,8 +87,40 @@ export default async function ({ addon, console }) {
 
 		const MediaInfoDate = document.createElement("span");
 		MediaInfoDate.classList.add("media-info-item", "date", "shortDateFormat");
-		MediaInfoDate.innerText = ``;
+		MediaInfoDate.innerText = GetShortestTimeDiff(modified);
+		
+		MediaInfo.append(MediaInfoTitleHolder, MediaInfoDate);
+		Holder.append(MediaThumbnailHolder, MediaInfo);
 
+		return Holder;
+	}
 
+	/**
+	 * Gets the difference between 2 dates
+	 * @param {Date} Date0
+	 * @param {Date} Date1
+	 * @param {String} locale
+	 */
+	function GetShortestTimeDiff(Date0, Date1=new Date(Date.now()), locale="en") {
+		const RTF = new Intl.RelativeTimeFormat(locale, { numeric: "always", style: "short" });
+
+		const DiffYears = Date1.getFullYear() - Date0.getFullYear();
+		const DiffMonths = Date1.getMonth() - Date0.getMonth();
+		const DiffDate = Date1.getDate() - Date0.getDate();
+		const DiffHours = Date1.getHours() - Date0.getHours();
+		const DiffMinutes = Date1.getMinutes() - Date0.getMinutes();
+		const DiffSeconds = Date1.getSeconds() - Date0.getSeconds();
+
+		if (DiffYears !== 0)
+			return RTF.format(DiffYears * -1, "year");
+		if (DiffMonths !== 0)
+			return RTF.format(DiffMonths * -1, "month");
+		if (DiffDate !== 0)
+			return RTF.format(DiffDate * -1, "day");
+		if (DiffHours !== 0)
+			return RTF.format(DiffHours * -1, "hours");
+		if (DiffMinutes !== 0)
+			return RTF.format(DiffMinutes * -1, "minute");
+		return RTF.format(DiffSeconds * -1, "seconds")
 	}
 }
